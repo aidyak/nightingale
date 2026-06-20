c27be9edcb7fcea7811ed8bb145503384a825ed3
aidyak <yusei.kaida@giftee.co>
2026-06-20 20:10:32 +0900

    add update related function

--- ext/nightingale/src/lib.rs
+++ ext/nightingale/src/lib.rs
@@ -12,8 +12,31 @@ lazy_static! {
     static ref STATE: Mutex<EngineState> = Mutex::new(EngineState { x: 100.0, y: 100.0 });
 }
 
-pub fn hello(subject: String) -> String {
-    format!("Hello {subject}, from Rust!")
+fn update_position(dx: f32, dy: f32) {
+    let mut state = STATE.lock().unwrap();
+    state.x += dx as f32;
+    state.y += dy as f32;
+
+    if state.x > 800.0 {
+        state.x = 0.0;
+    }
+    if state.y > 600.0 {
+        state.y = 0.0;
+    }
+}
+
+fn state_engine() {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(async {
+        request_new_screen_size(800, 600);
+        loop {
+            clear_background(WHITE);
+            let state = STATE.lock().unwrap();
+            draw_circle(state.x, state.y, 50.0, 50.0, RED);
+            drop(state);
+            next_frame().await;
+        }
+    });
 }
 
 #[magnus::init]
