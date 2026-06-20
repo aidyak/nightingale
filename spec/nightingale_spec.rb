b722460e68941d71132561aa4c5f16b49c76604c
aidyak <yusei.kaida@giftee.co>
2026-06-20 22:48:19 +0900

    delete unnecessary spec

--- spec/nightingale_spec.rb
+++ spec/nightingale_spec.rb
@@ -4,10 +4,4 @@ RSpec.describe Nightingale do
   it "has a version number" do
     expect(Nightingale::VERSION).not_to be nil
   end
-
-  it "can call into Rust" do
-    result = Nightingale.hello("world")
-
-    expect(result).to be("Hello earth, from Rust!")
-  end
 end
